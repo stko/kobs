0ac1b31ba5f757b4fe0ee7aaa2375ae1ae3c56db
steffen@koehlers.de <steffen@koehlers.de@80fb32fc-e0d5-11dd-9208-8f743628cd7a>
2009-01-18 19:14:55 +0000

    Abruf der passwortgeschützen Userdaten als XML- File funktioniert augenscheinlich

---
+++ trunk/config.php
@@ -0,0 +1,14 @@
+<?php
+/******************************************************************************
+ * Konfigurationsdatei von Admidio
+ *
+ * Copyright    : (c) 2009  The Admidio Team
+ * Homepage     : http://www.admidio.org
+ * License      : GNU Public License 2 http://www.gnu.org/licenses/gpl-2.0.html
+ *
+ *****************************************************************************/
+
+// Gibt den Namen der Rolle an, die ein User braucht, um die Funktionen des KOBS- Moduls nutzen zu dürfen
+
+$kobs_trainer = "Trainer";
+?>
