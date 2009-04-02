9384c9fb0c595d83428696b282b3067fba30c574
steffen@koehlers.de <steffen@koehlers.de@80fb32fc-e0d5-11dd-9208-8f743628cd7a>
2009-04-02 19:28:37 +0000

    Upload der Sessiondata ist jetzt soweit abgeschlossen, es fehlt nur noch die Auswertung auf der Serverseite

--- trunk/client/java/application/Kobs/src/kobs/KReadHTTPFile.java
+++ trunk/client/java/application/Kobs/src/kobs/KReadHTTPFile.java
@@ -31,23 +31,33 @@ public class KReadHTTPFile {
         }
     }
 
-    public static String readURL2File(String URL, String fileName, String userName, String passWd) {
+
+    public static String syncronize2URL(String URL, String sessionFileName, String userDataFileName, String userName, String passWd) {
         kuser = userName;
         kpass = passWd;
         Authenticator.setDefault(new MyAuthenticator());
         try {
             URL url = new URL(URL);
-            InputStream fis = url.openConnection().getInputStream();
-            try {
-                FileOutputStream fos = new FileOutputStream(fileName);
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("POST");
+            //conn.setUseCaches(false);
+            conn.setRequestProperty("Content-Type", "text/xml; charset=\"utf-8\"");
+            conn.setDoOutput(true);
+ //           conn.setDoInput(true);
+           // wenn man zeichensatz-konform schreiben möchte, sollte man einen passenden Stream nehmen:
+            //OutputStreamWriter out = new OutputStreamWriter(conn.getOutputStream(), "UTF8");
+            // wir lesen aber binär, und darum:
+            BufferedOutputStream out = new BufferedOutputStream(conn.getOutputStream());
+             DataInputStream fs = new DataInputStream(new FileInputStream(sessionFileName));
+            chain(fs,out);
+            out.close();
+           InputStream fis = conn.getInputStream();
+           try {
+                FileOutputStream fos = new FileOutputStream(userDataFileName);
 
                 try {
-                    byte[] buffer = new byte[0xFFFF];
-                    int len;
-                    while ((len = fis.read(buffer)) != -1) {
-                        fos.write(buffer, 0, len);
-                    }
-                } catch (IOException e) {
+                    chain(fis,fos);
+                 } catch (IOException e) {
                     return KobsApp.lang.getProperty("URLSaveText", "Couldn't save file to disk");
                 } finally {
                     if (fis != null) {
@@ -68,72 +78,13 @@ public class KReadHTTPFile {
             } catch (IOException e) {
                 return KobsApp.lang.getProperty("URLSaveText", "Couldn't save file to disk");
             }
-
-        } catch (java.net.MalformedURLException e) {
-            return KobsApp.lang.getProperty("URLErrorText", "Couldn't connect to Server- is the URL correct?");
-        } catch (java.io.IOException e) {
-            return KobsApp.lang.getProperty("URLDownloadErrorText", "Couldn't download data- please check login data (case sensitive)");
-        }
-        return "";
-    }
-
-    public static String writeFile2URL(String URL, String fileName, String userName, String passWd) {
-        kuser = userName;
-        kpass = passWd;
-        Authenticator.setDefault(new MyAuthenticator());
-        try {
-            URL url = new URL(URL);
-            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-            conn.setRequestMethod("POST");
-            //conn.setUseCaches(false);
-            conn.setRequestProperty("Content-Type", "text/xml; charset=\"utf-8\"");
-            conn.setDoOutput(true);
-           // wenn man zeichensatz-konform schreiben möchte, sollte man einen passenden Stream nehmen:
-            //OutputStreamWriter out = new OutputStreamWriter(conn.getOutputStream(), "UTF8");
-            // wir lesen aber binär, und darum:
-            BufferedOutputStream out = new BufferedOutputStream(conn.getOutputStream());
-            DataInputStream fs = new DataInputStream(new FileInputStream(fileName));
-            chain(fs,out);
-            out.close();
-
             if (conn.getResponseCode() != HttpURLConnection.HTTP_OK) {
                 return (conn.getResponseMessage());
             }
-        conn.disconnect();
+ 
+            conn.disconnect();
 
-        /* 
-        
-         *  
-        byte b[] = {};
-        try {
-        File f = new File(<Path2File>);
-        long len = f.length();
-        if (len == 0) throw new IOException("No file to write!");
-        DataInputStream fs = new DataInputStream(new FileInputStream(<Path2File>));
-        b = new byte[(int)len];
-        int bytes = fs.read(b);
-        fs.close();
-        if (bytes != len) throw new IOException("Error reading file, bytes read = "+bytes);
-        } catch (IOException e1) {
-        //log.error("File error!");
-        e1.printStackTrace();
-        return;
-        }
-        try {
-        URL url = "" URL(<UrlString>);
-        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-        conn.setDoOutput(true);
-        BufferedOutputStream output = new BufferedOutputStream(conn.getOutputStream());
-        output.write(b);
-        output.close();
-        int responseCode = conn.getResponseCode();
-        if (responseCode < 200 || responseCode > 299) {
-        throw new IOException("Error uploading file!" +  responseCode);
-        }
-        conn.disconnect();
-  
-        
-         */
+     
 
         } catch (java.net.MalformedURLException e) {
             return KobsApp.lang.getProperty("URLErrorText", "Couldn't connect to Server- is the URL correct?");
