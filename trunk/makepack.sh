1e66c57f0b5f94af86adde671c998ac9f56f6c81
steffen@koehlers.de <steffen@koehlers.de@80fb32fc-e0d5-11dd-9208-8f743628cd7a>
2009-07-04 15:22:46 +0000

    SerialReader erweitert für Cardreader mit binärem (Raw) Datenformat

--- trunk/makepack.sh
+++ trunk/makepack.sh
@@ -5,6 +5,7 @@ mkdir /media/ram/klobs
 cp -r client/java/application/Kobs/dist/* /media/ram/klobs
 cp client/java/application/Kobs/Klobslogo* /media/ram/klobs
 cp client/java/interface/serial/*.class client/java/interface/serial/*.lang client/java/interface/serial/*.props  /media/ram/klobs
+cp client/java/application/Kobs/*.lang client/java/application/Kobs/*.props  /media/ram/klobs
 cp client/java/interface/serial/lib/* /media/ram/klobs/lib
 cp runKlobs.sh /media/ram/klobs/
 cp Klobs.desktop /media/ram/klobs/
