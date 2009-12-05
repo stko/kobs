b72e7fdc70ccab2bb7d49f62e98ec7c4853ef1af
steffen@koehlers.de <steffen@koehlers.de@80fb32fc-e0d5-11dd-9208-8f743628cd7a>
2009-12-05 05:54:16 +0000

    Jetzt werden nur noch Mitglieder der Rolle "Mitglied" zum Client übertragen, aber nicht mehr sonstige User in der Datenbank, wie z.B. Ehemalige
    Zur einfacheren Pflege wurden die bisher im Source hardgecodeten Daten für Orte und Trainingsaktivitäten in die externen XML- Dateien "trainings" und "locations" ausgelagert

--- config.php
+++ config.php
@@ -9,15 +9,18 @@
  *****************************************************************************/
 
 // Gibt den Namen der Rolle an, die ein User braucht, um die Funktionen des klobs- Moduls nutzen zu dürfen
-
 $klobs_trainer = "Trainer";
+
+// Gibt den Namen der Rolle an, die ein User braucht, damit er im Klobs-Client aufgelistet wird und so
+// z.B. Ehemalige unterdrückt werden 
+$klobs_member = "Mitglied";
+
 // Gibt den Namen des Feldes an, in der sich die Kartennummer befindet
-
 $klobs_card = "Kartennummer";
+
 // Gibt den Namen des Feldes an, in der sich die zweite Klassifizierung befindet
-
 $klobs_belt = "Gurt";
-// Gibt den Tabellennamen an, in dem sich die Trainingseinheiten befinden
 
+// Gibt den Tabellennamen an, in dem sich die Trainingseinheiten befinden
 $klobs_training_table = "adm_klobs_training";
 ?>
