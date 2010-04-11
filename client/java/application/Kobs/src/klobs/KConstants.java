dbaddcd8bb94076419387e7e48c876aa46838b7d
steffen@koehlers.de <steffen@koehlers.de@80fb32fc-e0d5-11dd-9208-8f743628cd7a>
2010-04-11 08:02:31 +0000

    Anwesenheits- Auswahlfenster jetzt auch mit Gruppenaufteilung und sortierter Auflistung

--- client/java/application/Kobs/src/klobs/KConstants.java
+++ client/java/application/Kobs/src/klobs/KConstants.java
@@ -20,6 +20,7 @@ public class KConstants {
     public final static String LocIdName = "ort_id";
     public final static String UsrIdName = "usr_id";
     public final static String MemModKey = "modified";
+    public final static String MemGroup = "gurt";
     public final static String MemOnside = "onside";
     public final static String TrueValue = "true";
     public final static String FalseValue = "false";
