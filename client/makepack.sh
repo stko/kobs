878de0e7955ecb7a795b6e381945ff4d31f76601
Steffen Köhler <steffen@koehlers.de>
2020-02-15 12:25:32 +0100

    Make Installer aktualisiert

--- client/makepack.sh
+++ client/makepack.sh
@@ -2,13 +2,13 @@
 rm -rf /media/ram/klobs
 rm /media/ram/klops.zip
 mkdir /media/ram/klobs
-cp -r client/java/application/Kobs/dist/* /media/ram/klobs
-cp client/java/application/Kobs/Klobslogo* /media/ram/klobs
-cp client/java/interface/serial/*.jar client/java/interface/serial/*.lang client/java/interface/serial/*.props  client/java/interface/serial/*.xpm /media/ram/klobs
-cp client/java/interface/qrcode/*.jar client/java/interface/qrcode/*.lang client/java/interface/qrcode/*.props  client/java/interface/qrcode/*.xpm /media/ram/klobs
-cp client/java/application/Kobs/*.lang  /media/ram/klobs
-cp client/java/interface/qrcode/lib/* /media/ram/klobs/lib
-cp client/java/interface/serial/lib/* /media/ram/klobs/lib
+cp -r java/application/Kobs/dist/* /media/ram/klobs
+cp java/application/Kobs/Klobslogo* /media/ram/klobs
+cp java/interface/serial/*.jar java/interface/serial/*.lang java/interface/serial/*.props  java/interface/serial/*.xpm /media/ram/klobs
+cp java/interface/qrcode/*.jar java/interface/qrcode/*.lang java/interface/qrcode/*.props  java/interface/qrcode/*.xpm /media/ram/klobs
+cp java/application/Kobs/*.lang  /media/ram/klobs
+cp java/interface/qrcode/lib/* /media/ram/klobs/lib
+cp java/interface/serial/lib/* /media/ram/klobs/lib
 cp run*.sh /media/ram/klobs/
 cp *.desktop /media/ram/klobs/
 cp shojikido_logo_h480.png /media/ram/klobs/
@@ -19,4 +19,6 @@ cp installklobs.sh /media/ram/klobs/
 makensis klobs_windows_setup.nsi
 zip KLOBS_Linux_Setup.zip KLOBS_Installer_LINUX.run
 zip KLOBS_Windows_Setup.zip KLOBS_Windows_Setup.exe
+DIR=$(pwd)
+(cd .. ; zip $DIR/KLOBS_Server_Setup.zip -r adm_plugins)
 
